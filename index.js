ad6ee27e3e26c526269636e9c024cefd0b5c585c
Tim Lantz <tlantz@gmail.com>
2021-01-09 13:10:41 -0600

    add google user auth with passport, install nodemon

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-  res.send({hi: 'I am running on heroku!'});
-});
+const app = express();
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = keys;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken: ', accessToken);
+      console.log('refreshToken: ', refreshToken);
+      console.log('profile: ', profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get(
+  '/auth/google/callback', 
+  passport.authenticate('google')
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
