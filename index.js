48f9b5225c4923a449cf18325032b846394e4ed6
Tim Lantz <tlantz@gmail.com>
2021-01-11 16:34:28 -0600

    install mongoose and create basic User model

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.MONGO_URI);
 
 const app = express();
 
