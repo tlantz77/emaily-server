52798f141f563303fb20c508b6f4d4473cb5c4d1
Tim Lantz <tlantz@gmail.com>
2021-01-11 18:36:06 -0600

    finish auth

--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,16 @@ const mongoose = require('mongoose');
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = keys;
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -14,9 +24,9 @@ passport.use(
       callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id}).then(user => {
-        if (user) {
-          done(null, user);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
         } else {
           new User({ googleId: profile.id }).save().then(user => {
             done(null, user);
