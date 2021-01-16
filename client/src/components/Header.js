b8c2191c3294cede72980f38932cf4a100d5877e
Tim Lantz <tlantz@gmail.com>
2021-01-16 13:59:02 -0600

    make Header jandle auth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return <li><a href='/auth/google'>Login With Google</a></li>;
+      default:
+        return <li><a>Welcome, User!  Logout</a></li>
+    }
+  }
+
   render() { 
     return (
       <nav>
         <div className='nav-wrapper'>
           <a className='left brand-logo'>Emaily</a>
           <ul className='right'>
-            <li><a>Login With Google</a></li>
+            { this.renderContent() }
           </ul>
         </div>
       </nav>
     );
   }
 }
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
  
-export default Header;
+export default connect(mapStateToProps)(Header);
